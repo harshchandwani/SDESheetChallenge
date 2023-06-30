3199f55b759d87ada23214713e4efe2cf9748769
Harsh Chandwani <67815775+harshchandwani@users.noreply.github.com>
2023-06-30 19:32:18 +0530

    Create Find Median from Data Stream.cpp

---
+++ Find Median from Data Stream.cpp
@@ -0,0 +1,28 @@
+class MedianFinder {
+public:
+    priority_queue<int> maxh;
+    priority_queue<int,vector<int>,greater<int>> minh;
+    /*
+        1) store first half in maxh, second half in minh
+        2) maxh can have 1 element more than minh
+    */
+    void addNum(int num) {
+        maxh.push(num);
+        minh.push(maxh.top());
+        maxh.pop();
+        if(minh.size()>maxh.size()){
+            maxh.push(minh.top());
+            minh.pop();
+        }
+    }
+    double findMedian() {
+        if(maxh.size()==minh.size()) return (maxh.top()+minh.top())/2.0;
+        return maxh.top();
+    }
+};
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
